1ffecd45da97496d384c9641e24f85cabc131965
Roman Stratiienko <r.stratiienko@gmail.com>
2021-06-23 17:11:10 +0300

    minigbm: Add bo_get_plane_fd backend hook
    Non-DRM drivers shouldn't rely on handles and DRM API.
    Add hook to allow drivers create custom implementation.
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>
    Change-Id: I458dae38f80697184070019606b125992a9aa01d

--- drv.c
+++ drv.c
@@ -699,6 +699,10 @@ int drv_bo_get_plane_fd(struct bo *bo, size_t plane)
 	if (bo->is_test_buffer)
 		return -EINVAL;
 
+	if (bo->drv->backend->bo_get_plane_fd) {
+		return bo->drv->backend->bo_get_plane_fd(bo, plane);
+	}
+
 	return bo->drv->backend->bo_export(bo, plane);
 }
 
