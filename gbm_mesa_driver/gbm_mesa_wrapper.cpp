0128513a6365e02c06ee8e926676bcf90dd4b341
Alessandro Astone <ales.astone@gmail.com>
2025-05-18 21:33:04 +0200

    gbm_mesa: Replace GBM_BO_USE_LINEAR with DRM_FORMAT_MOD_LINEAR
    GBM_BO_USE_LINEAR is invalid in gbm_bo_create_with_modifiers2(), so we were
    falling back to gbm_bo_create().
    gbm_bo_create() itself returns a buffer with DRM_FORMAT_MOD_INVALID which
    some compositors refuse to accept.
    
    Closes: https://github.com/waydroid/waydroid/issues/1887

--- gbm_mesa_driver/gbm_mesa_wrapper.cpp
+++ gbm_mesa_driver/gbm_mesa_wrapper.cpp
@@ -128,9 +128,14 @@ static int gbm_mesa_alloc(struct alloc_args *args)
 	if (args->use_scanout)
 		usage |= GBM_BO_USE_SCANOUT;
 
-	const std::vector<uint64_t> modifiers = get_supported_modifiers(args->gbm, args->drm_format);
+	std::vector<uint64_t> modifiers = get_supported_modifiers(args->gbm, args->drm_format);
 	if (modifiers.size() > 0) {
-		bo = gbm_bo_create_with_modifiers2(args->gbm, args->width, args->height, gbm_format, modifiers.data(), modifiers.size(), usage);
+		int usage2 = usage;
+		if (usage2 & GBM_BO_USE_LINEAR) {
+			usage2 &= ~GBM_BO_USE_LINEAR;
+			modifiers = {DRM_FORMAT_MOD_LINEAR};
+		}
+		bo = gbm_bo_create_with_modifiers2(args->gbm, args->width, args->height, gbm_format, modifiers.data(), modifiers.size(), usage2);
 	}
 	if (!bo) {
 		ALOGV("fallback to gbm_bo_create without modifiers");
